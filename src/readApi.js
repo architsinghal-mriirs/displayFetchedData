b6c7ed88fdf2fd6364e4d7ee50498ae5e0f9720b
Sumit <schandilao1>
2022-11-20 12:25:10 +0530

    Initial Commit

---
+++ src/readApi.js
@@ -0,0 +1,21 @@
+// const fs = require('fs');
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+ 
+// // const staticPath = path.join(__dirname, "../Api.json");
+// // console.log(staticPath);
+// // app.use(express.static(staticPath));
+
+// fs.readFileSync('../Api.json', 'utf-8', (err , data)=>{
+//     console.log(data);
+// })
+
+// app.listen(80, ()=>{
+//     console.log("Server Is Running");
+// });
+
+
+
+
+ 
